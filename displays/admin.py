60d9d3e80c414788a15ab4addca96333f2e9c814
c160034 <chiawei960229@gmail.com>
2022-10-20 13:27:27 +0800

    renaming model Author to User

--- displays/admin.py
+++ displays/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import Listing, Author
+from .models import Listing, User
 
 # Register your models here.
 
 class ListingAdmin(admin.ModelAdmin):
-    list_filter = ("author", "date",)
-    list_display = ("title", "date", "author",)
+    list_filter = ("user", "date",)
+    list_display = ("title", "date", "user",)
     prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Listing, ListingAdmin)
-admin.site.register(Author)
+admin.site.register(User)
--- displays/models.py
+++ displays/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Author(models.Model):
+class User(models.Model):
     username = models.CharField(max_length=50)
     email_address = models.EmailField()
 
@@ -15,4 +15,4 @@ class Listing(models.Model):
     date = models.DateField(auto_now_add=True)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, max_length=250)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="listings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
